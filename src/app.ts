b8d88c470cfcc3d102033c6787b7db7da5b45638
Chris Nelson <superchrisnelson@gmail.com>
2015-12-18 20:53:59 -0500

    Trying to bring in n2-cli-test-lib

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {AngularCliTestApp} from './app/angular-cli-test';
-
+// import PhoenixChannels from "angular2-phoenix-channels";
 
 bootstrap(AngularCliTestApp);
--- src/app/angular-cli-test.ts
+++ src/app/angular-cli-test.ts
@@ -1,16 +1,16 @@
 import {Component} from 'angular2/core';
-
+import ng2test from "ng2-cli-test-lib";
 
 @Component({
   selector: 'angular-cli-test-app',
-  providers: [],
+  providers: [ng2test.providers],
   templateUrl: 'app/angular-cli-test.html',
   directives: [],
   pipes: []
 })
 export class AngularCliTestApp {
   defaultMeaning: number = 42;
-  
+
   meaningOfLife(meaning) {
     return `The meaning of life is ${meaning || this.defaultMeaning}`;
   }
